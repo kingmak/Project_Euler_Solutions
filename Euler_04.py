1070ecd329abcd3043d2081164f25ced2e5aa19f
King Mak <kingmak@hotmail.com>
2013-12-02 07:47:51 -0600

    Problem 4 Project Euler

---
+++ Euler_04.py
@@ -0,0 +1,28 @@
+#Problem 4 Project Euler
+from collections import OrderedDict
+
+Palindromes = []
+Products = []
+
+def FindProducts():
+        global Products
+        for i in range (100, 1000):
+                for j in range (100, 1000):
+                        Products.append(i * j)
+                        j += 1
+                i += 1
+        Products = list (OrderedDict.fromkeys(Products))
+
+def FindPalindromes():
+        count = 0
+        while count < len(Products):
+                if str(Products[count]) == str(Products[count])[::-1]:
+                        Palindromes.append(Products[count])
+                count += 1
+                
+FindProducts()
+FindPalindromes()
+LenPalindromes = len(Palindromes)
+LargestPalindrome =  sorted(Palindromes)[LenPalindromes - 1]
+print LargestPalindrome
+raw_input('')
