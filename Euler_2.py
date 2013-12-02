a9bfc2a1e8ece08807d4a57d1ef69d8db73ad1b0
King Mak <kingmak@hotmail.com>
2013-12-01 19:24:42 -0600

    Create Euler_2.py

---
+++ Euler_2.py
@@ -0,0 +1,22 @@
+#
+FiboNums = []
+FiboEvenNums = []
+
+def FiboSeq(end):
+    first, second = 0, 1
+    while second < end:
+        FiboNums.append(second)
+        first, second = second, first + second
+
+def Comparison():
+    count = 0
+    while count < len(FiboNums):
+        if FiboNums[count] % 2 == 0:
+            FiboEvenNums.append(FiboNums[count])
+        count += 1
+
+            
+FiboSeq(4000000)
+Comparison()
+print sum(FiboEvenNums)
+raw_input('')
