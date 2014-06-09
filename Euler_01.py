f31292eba36029f7c5ab7e3e1806485ff80ce1bb
King Mak <kingmak@hotmail.com>
2014-06-09 13:19:11 -0500

    Found a shorter algorithm

--- Euler_01.py
+++ Euler_01.py
@@ -1,21 +1,6 @@
 #Problem 1 Project Euler
-from collections import OrderedDict
-
-def FindMultiples(num, Array):
-    count = 0
-    while count < len(NumStorage):
-        if NumStorage[count] % num == 0:
-            Array.append(NumStorage[count])
-        count += 1
-        
-NumStorage = list(range(0, 1000))
-Num3s = []; Num5s = []; SumMultiples = []
-                             
-FindMultiples(3, Num3s); FindMultiples(5, Num5s)
-
-SumMultiples = Num3s + Num5s
-SumMultiples = list (OrderedDict.fromkeys(SumMultiples))
-SumMultiples = sum(SumMultiples)
-
-print SumMultiples
-raw_input('')
+Multiples = []
+for num in range(0, 1000):
+	if num % 3 == 0 or num % 5 == 0:
+		Multiples.append(num)
+print sum(Multiples)
