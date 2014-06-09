9a496b84ed542eb35a8458915687eecf700f24ec
King Mak <kingmak@hotmail.com>
2014-06-09 14:32:56 -0500

    Update Euler_04.py

--- Euler_04.py
+++ Euler_04.py
@@ -1,26 +1,9 @@
 #Problem 4 Project Euler
-
-Palindromes = []
-Products = []
-
-def FindProducts():
-        global Products
-        for i in range (100, 1000):
-                for j in range (100, 1000):
-                        Products.append(i * j)
-                        j += 1
-                i += 1
-
-def FindPalindromes():
-        count = 0
-        while count < len(Products):
-                if str(Products[count]) == str(Products[count])[::-1]:
-                        Palindromes.append(Products[count])
-                count += 1
-                
-FindProducts()
-FindPalindromes()
-LenPalindromes = len(Palindromes)
-LargestPalindrome =  sorted(Palindromes)[LenPalindromes - 1]
-print LargestPalindrome
-raw_input('')
+palindrome = 0
+for numOne in range(100, 1000):
+	for numTwo in range(100, 1000):
+		numba = numOne * numTwo
+		if str(numba) == str(numba)[::-1]:
+			if numba > palindrome:
+					palindrome = numba
+print 'larger palindrom = %d ' % palindrome
