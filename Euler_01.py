8c440a51fc6d836ded4a99c9219209152d3dfdc2
King Mak <kingmak@hotmail.com>
2014-06-10 13:52:21 -0500

    Update Euler_01.py

--- Euler_01.py
+++ Euler_01.py
@@ -1,6 +1,5 @@
 ##Problem 1 Project Euler
 Multiples = 0
 for num in range(0, 1000):
-	if num % 3 == 0 or num % 5 == 0:
-		Multiples += num
+	if num % 3 == 0 or num % 5 == 0: Multiples += num
 print Multiples
