4424a51e3d9cbd1911c34dd5c7d8161eb675a513
King Mak <kingmak@hotmail.com>
2014-06-10 06:14:56 -0500

    Update Euler_01.py
    used a var to sum value of the multiples instead of an array

--- Euler_01.py
+++ Euler_01.py
@@ -1,6 +1,6 @@
 ##Problem 1 Project Euler
-Multiples = []
+Multiples = 0
 for num in range(0, 1000):
 	if num % 3 == 0 or num % 5 == 0:
-		Multiples.append(num)
-print sum(Multiples)
+		Multiples += num
+print Multiples
