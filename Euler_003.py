539fc813c243551332ef9def81e0cad959de6d1d
kingmak <kingmak@hotmail.com>
2016-05-15 17:01:43 -0500

    Update Euler_003.py

--- Euler_003.py
+++ Euler_003.py
@@ -1,6 +1,6 @@
 #Problem 3 Project Euler
 x, num = 2, 600851475143
 while num != x:
-    if num % x == 0: num = num / x; x = 2
-    else: x += 1
+    if num % x == 0: num = num / x
+    x += 1
 print x
