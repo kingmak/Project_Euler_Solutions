04b8a476e9c59f6022e0968579b465f11ac6d4bb
KingMak <KingMak@SomeFakeEmail.com>
2017-10-22 03:35:17 -0500

    made the code a lil faster by only checking odds, 2 added in prime array by default

--- Euler_007.py
+++ Euler_007.py
@@ -1,3 +1,4 @@
+'''
 def isPrime(n):
     return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))  
 
@@ -8,4 +9,17 @@ while len(primes) != 10001:
     if isPrime(index):
         primes.append(index)
     index += 1
+print primes[-1]
+'''
+
+def isPrime(n):
+    return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))  
+
+index = 3
+primes = [2] # added 2 since its the only even prime
+
+while len(primes) != 10001:
+    if isPrime(index):
+        primes.append(index)
+    index += 2
 print primes[-1]
