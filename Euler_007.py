2b5d97810e726c3ea0faf6f4ad26a33c6aeb6aae
KingMak <KingMak@SomeFakeEmail.com>
2017-10-22 03:51:11 -0500

    added better primality check

--- Euler_007.py
+++ Euler_007.py
@@ -13,10 +13,22 @@ print primes[-1]
 '''
 
 def isPrime(n):
-    return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))  
+    # if n <= 1: return False
+    # if n <= 3: return True
+
+    if (n % 2 == 0) or (n % 3 == 0):
+        return False
+
+    index = 5
+    while (index * index) <= n:
+        if (n % index == 0) or ((n % (index + 2)) == 0):
+            return False
+        index += 6
+
+    return True
 
 index = 3
-primes = [2] # added 2 since its the only even prime
+primes = [2, 3] # added 2 since its the only even prime
 
 while len(primes) != 10001:
     if isPrime(index):
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Project Euler Solutions
 =======================
 
-As I work Through the [Project Euler](https://projecteuler.net) Problems I Will Post the Solutions Here in Python.
+As I work Through the [Project Euler](https://projecteuler.net) Problems I Will Post the Solutions Here in Python.<br>
+(primality check algo is in Euler007)
  
